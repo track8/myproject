c4a1202c7fd25b6cc60bee57678ca162ec41cbb5
Yuichi NANSAI <nansai@nifty.com>
2017-08-23 01:17:07 +0900

    Start, Stop, Reset追加

--- myproject/ViewController.cs
+++ myproject/ViewController.cs
@@ -8,32 +8,80 @@ namespace myproject
     public partial class ViewController : NSViewController
     {
         System.Timers.Timer timer;
+        int timerRemainSeconds;
 
         public ViewController(IntPtr handle) : base(handle)
         {
         }
 
+        NSObject NSWindowDidResizeNotificationObject;
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
-            var seconds = 60 * 1;
+            NSWindowDidResizeNotificationObject = NSNotificationCenter.DefaultCenter.AddObserver(new NSString("NSWindowDidResizeNotification"), ResizeObserver, null);
+
+            timerRemainSeconds = InitialRemainSeconds;
+            this.label2.StringValue = timerString;
 
             timer = new System.Timers.Timer();
+            timer.Enabled = false;
             timer.Interval = 1000;
             timer.Elapsed += (sender, e) =>
             {
-                var ts = new TimeSpan(0, 0, seconds -= 1);
-
-                if (ts.TotalSeconds <= 0)
+                if (timerRemainSeconds <= 0)
                 {
                     timer.Stop();
                 }
 
-                InvokeOnMainThread(() => this.label.Title = ts.ToString("c"));
+                InvokeOnMainThread(() => this.label2.StringValue = timerString);
+
+                timerRemainSeconds -= 1;
             };
-            timer.Start();
-		}
+        }
+
+        partial void startButton_clicked(NSObject sender)
+        {
+            this.timer.Enabled = true;
+        }
+
+        partial void stopButton_clicked(NSObject sender)
+        {
+            this.timer.Enabled = false;
+        }
+
+        partial void resetButton_clicked(NSObject sender)
+        {
+            timerRemainSeconds = InitialRemainSeconds;
+            this.label2.StringValue = this.timerString;
+        }
+
+        private string timerString
+        {
+            get { return (new TimeSpan(0, 0, timerRemainSeconds)).ToString(("c")); }
+        }
+
+        private int InitialRemainSeconds
+        {
+            get
+            {
+                try
+                {
+                    return Convert.ToInt32(minuteTextBox.StringValue) * 60;
+                }
+                catch
+                {
+                    return 30 * 60;
+                }
+            }
+        }
+
+        public void ResizeObserver(NSNotification notify)
+        {
+            var r = this.View.Frame;
+            Console.WriteLine("{0}:{1}:{2}", notify.Name, r.Height, r.Width);
+        }
 
         public override NSObject RepresentedObject
         {
--- myproject/ViewController.designer.cs
+++ myproject/ViewController.designer.cs
@@ -13,13 +13,54 @@ namespace myproject
     partial class ViewController
     {
         [Outlet]
-        AppKit.NSTextFieldCell label { get; set; }
+        AppKit.NSTextField label2 { get; set; }
+
+        [Outlet]
+        AppKit.NSTextField minuteTextBox { get; set; }
+
+        [Outlet]
+        AppKit.NSButton ResetButton { get; set; }
+
+        [Outlet]
+        AppKit.NSButton StartButton { get; set; }
+
+        [Outlet]
+        AppKit.NSButton StopButton { get; set; }
+
+        [Action ("resetButton_clicked:")]
+        partial void resetButton_clicked (Foundation.NSObject sender);
+
+        [Action ("startButton_clicked:")]
+        partial void startButton_clicked (Foundation.NSObject sender);
+
+        [Action ("stopButton_clicked:")]
+        partial void stopButton_clicked (Foundation.NSObject sender);
         
         void ReleaseDesignerOutlets ()
         {
-            if (label != null) {
-                label.Dispose ();
-                label = null;
+            if (label2 != null) {
+                label2.Dispose ();
+                label2 = null;
+            }
+
+            if (StartButton != null) {
+                StartButton.Dispose ();
+                StartButton = null;
+            }
+
+            if (StopButton != null) {
+                StopButton.Dispose ();
+                StopButton = null;
+            }
+
+            if (ResetButton != null) {
+                ResetButton.Dispose ();
+                ResetButton = null;
+            }
+
+            if (minuteTextBox != null) {
+                minuteTextBox.Dispose ();
+                minuteTextBox = null;
             }
         }
     }
